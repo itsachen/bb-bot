90772e607b4fa15375def044cea2dc857985d1b5
Anthony Chen <pandaman0529@gmail.com>
2012-10-15 14:46:00 -0400

    bot.py made

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Bejeweled Blitz Bot
+
+## Requirements
+- Python 2.5+
+- autopy
---
+++ bot.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python2.7
+
+import autopy, sys, math, time, random
+
+print "ALL YOUR BASE ARE BELONG TO US"
